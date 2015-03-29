0cbc8d9d39067d4935dcf6fc686a48d7954e5828
Nicolás Melgarejo Sabelle <NicoMelgarejo@MacBookAir.local>
2015-03-29 03:04:00 -0300

    deadline

--- db/migrate/20150328233441_add_deadline_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddDeadlineToProduct < ActiveRecord::Migration
-  def change
-    add_column :products, :deadline, :datetime, :default => 2.hours
-  end
-end
--- db/migrate/20150329001542_remove_deadline_from_product.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeadlineFromProduct < ActiveRecord::Migration
-  def change
-    remove_column :products, :deadline, :datetime
-  end
-end
